12981bc9320d993cf7da6825aa221f07b6d62883
shivenducs1136 <shivenduaps986@gmail.com>
2023-03-14 19:54:41 +0530

    agent screen done

--- lib/screens/admin/add_agent.dart
+++ lib/screens/admin/add_agent.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 
+import "../../main.dart";
+
 class AddAgentScreen extends StatefulWidget {
   const AddAgentScreen({super.key});
 
@@ -8,8 +10,225 @@ class AddAgentScreen extends StatefulWidget {
 }
 
 class _AddAgentScreenState extends State<AddAgentScreen> {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Form(
+            key: formKey,
+            child: SingleChildScrollView(
+              // ignore: prefer_const_literals_to_create_immutables
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(
+                            Icons.arrow_back,
+                            color: Colors.black,
+                            size: 25,
+                          ),
+                        ),
+                        const Text(
+                          "   Add Agent",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: mq.height * .04,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Agent Name",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18),
+                        ),
+                        SizedBox(
+                          height: mq.height * .01,
+                        ),
+                        SizedBox(
+                          height: mq.height * .05,
+                          child: TextFormField(
+                            keyboardType: TextInputType.name,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12)),
+                              hintText: "eg. John Slicky",
+                              labelText: "Agent Name",
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: mq.height * .01,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Age",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: mq.height * .01,
+                            ),
+                            SizedBox(
+                              height: mq.height * .05,
+                              width: mq.width * .3,
+                              child: TextFormField(
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(12)),
+                                  hintText: "eg. 21,22,23",
+                                  labelText: "Age",
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          width: mq.width * .05,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Phone Number",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: mq.height * .01,
+                            ),
+                            SizedBox(
+                              height: mq.height * .05,
+                              width: mq.width * .5,
+                              child: TextFormField(
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(12)),
+                                  hintText: "eg. 9988776655",
+                                  labelText: "Phone Number",
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: mq.height * .01,
+                    ),
+                    SizedBox(
+                      height: mq.height * .01,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Address",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: mq.height * .01,
+                    ),
+                    SizedBox(
+                      height: mq.height * .15,
+                      width: mq.width * 1,
+                      child: TextFormField(
+                        keyboardType: TextInputType.streetAddress,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12)),
+                          hintText: "eg. XYZ street",
+                        ),
+                        maxLines: 4,
+                      ),
+                    ),
+                    SizedBox(
+                      width: mq.width * .1,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              "Add Images",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18),
+                            ),
+                            SizedBox(
+                              width: mq.width * .1,
+                            ),
+                            Image.asset(
+                              "images/addimg.png",
+                              height: 50,
+                              width: 50,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: mq.height * .1,
+                    ),
+                    Container(
+                      height: mq.height * .07,
+                      width: mq.width,
+                      child: Center(
+                        child: Text(
+                          "Add Agent",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.blue),
+                    )
+                  ]),
+            ),
+          ),
+        ),
+      ),
+    );
+    ;
   }
 }
