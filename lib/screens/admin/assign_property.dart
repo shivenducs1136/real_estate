e687ae1f4b9d178db1ce5da6ea34bac62b7a7d1d
shivenducs1136 <shivenduaps986@gmail.com>
2023-03-14 19:57:52 +0530

    scaffold added

--- lib/screens/admin/assign_property.dart
+++ lib/screens/admin/assign_property.dart
@@ -10,6 +10,41 @@ class AssignPropertyScreen extends StatefulWidget {
 class _AssignPropertyScreenState extends State<AssignPropertyScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            // ignore: prefer_const_literals_to_create_immutables
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Row(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Icon(
+                      Icons.arrow_back,
+                      color: Colors.black,
+                      size: 25,
+                    ),
+                  ),
+                  const Text(
+                    "  Assign property to agent",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ]),
+          ),
+        ),
+      ),
+    );
+    ;
   }
 }
--- lib/screens/admin/view_property.dart
+++ lib/screens/admin/view_property.dart
@@ -13,6 +13,41 @@ class ViewPropertyScreen extends StatefulWidget {
 class _ViewPropertyScreenState extends State<ViewPropertyScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            // ignore: prefer_const_literals_to_create_immutables
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Row(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Icon(
+                      Icons.arrow_back,
+                      color: Colors.black,
+                      size: 25,
+                    ),
+                  ),
+                  const Text(
+                    "  All Properties",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ]),
+          ),
+        ),
+      ),
+    );
+    ;
   }
 }
