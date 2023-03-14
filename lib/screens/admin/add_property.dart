ad956cde97ce12e92ba5fafd1008ec21e3af6765
shivenducs1136 <shivenduaps986@gmail.com>
2023-03-14 19:37:20 +0530

    add property form created

--- lib/screens/admin/add_property.dart
+++ lib/screens/admin/add_property.dart
@@ -15,102 +15,355 @@ class _AddPropertyScreenState extends State<AddPropertyScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Form(
-          key: formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(children: [
-                  Icon(
-                    Icons.arrow_back,
-                    size: 24,
-                    color: Colors.black,
-                  ),
-                  Text(
-                    "  Add Property",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 24,
-                        fontWeight: FontWeight.w500),
-                  )
-                ]),
-                SizedBox(
-                  height: mq.height * .05,
-                ),
-                Text(
-                  "Property Name",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500),
-                ),
-                SizedBox(
-                  height: mq.height * .01,
-                ),
-                TextFormField(
-                  initialValue: null,
-                  onSaved: (val) => {},
-                  validator: (val) =>
-                      val != null && val.isNotEmpty ? null : "Required Field",
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12)),
-                    hintText: 'eg. feeling happy!',
-                    label: Text("About"),
-                  ),
-                ),
-                SizedBox(
-                  height: mq.height * .01,
-                ),
-                Row(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Form(
+            key: formKey,
+            child: SingleChildScrollView(
+              // ignore: prefer_const_literals_to_create_immutables
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    Row(
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(
+                            Icons.arrow_back,
+                            color: Colors.black,
+                            size: 25,
+                          ),
+                        ),
+                        const Text(
+                          "   Add Property",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: mq.height * .04,
+                    ),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Bedrooms"),
-                        TextFormField(
-                          initialValue: null,
-                          onSaved: (val) => {},
-                          validator: (val) => val != null && val.isNotEmpty
-                              ? null
-                              : "Required Field",
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12)),
-                            hintText: 'eg. 1,2,3',
-                            label: Text("Bedrooms"),
+                        Text(
+                          "Property Name",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18),
+                        ),
+                        SizedBox(
+                          height: mq.height * .01,
+                        ),
+                        SizedBox(
+                          height: mq.height * .05,
+                          child: TextFormField(
+                            keyboardType: TextInputType.name,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12)),
+                              hintText: "eg. XYZ Villa",
+                              labelText: "Property Name",
+                            ),
                           ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: mq.height * .01,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Bedrooms",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: mq.height * .01,
+                            ),
+                            SizedBox(
+                              height: mq.height * .05,
+                              width: mq.width * .3,
+                              child: TextFormField(
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(12)),
+                                  hintText: "eg. 1,2,3",
+                                  labelText: "Bedrooms",
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          width: mq.width * .1,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Bedrooms",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: mq.height * .01,
+                            ),
+                            SizedBox(
+                              height: mq.height * .05,
+                              width: mq.width * .3,
+                              child: TextFormField(
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(12)),
+                                  hintText: "eg. 1,2,3",
+                                  labelText: "Bedrooms",
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: mq.height * .01,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Garages",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: mq.height * .01,
+                            ),
+                            SizedBox(
+                              height: mq.height * .05,
+                              width: mq.width * .3,
+                              child: TextFormField(
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(12)),
+                                  hintText: "eg. 1,2,3",
+                                  labelText: "Garages",
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          width: mq.width * .1,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Area",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: mq.height * .01,
+                            ),
+                            SizedBox(
+                              height: mq.height * .05,
+                              width: mq.width * .3,
+                              child: TextFormField(
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(12)),
+                                  hintText: "eg. 32 sq.ft.",
+                                  labelText: "Area in sq. ft.",
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: mq.height * .01,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Status",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: mq.height * .01,
+                            ),
+                            SizedBox(
+                              height: mq.height * .05,
+                              width: mq.width * .3,
+                              child: TextFormField(
+                                keyboardType: TextInputType.name,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(12)),
+                                  hintText: "eg. 1,2,3",
+                                  labelText: "Status",
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          width: mq.width * .1,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Year built",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: mq.height * .01,
+                            ),
+                            SizedBox(
+                              height: mq.height * .05,
+                              width: mq.width * .3,
+                              child: TextFormField(
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(12)),
+                                  hintText: "eg. 2012",
+                                  labelText: "Year Build",
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: mq.height * .01,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Address",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18),
                         ),
                       ],
+                    ),
+                    SizedBox(
+                      height: mq.height * .01,
+                    ),
+                    SizedBox(
+                      height: mq.height * .15,
+                      width: mq.width * 1,
+                      child: TextFormField(
+                        keyboardType: TextInputType.streetAddress,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12)),
+                          hintText: "eg. XYZ street",
+                        ),
+                        maxLines: 4,
+                      ),
+                    ),
+                    SizedBox(
+                      width: mq.width * .1,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              "Add Images",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18),
+                            ),
+                            SizedBox(
+                              width: mq.width * .1,
+                            ),
+                            Image.asset(
+                              "images/addimg.png",
+                              height: 50,
+                              width: 50,
+                            ),
+                          ],
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text("GPS Location"),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: mq.height * .1,
+                    ),
+                    Container(
+                      height: mq.height * .07,
+                      width: mq.width,
+                      child: Center(
+                        child: Text(
+                          "Add Property",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.blue),
                     )
-                  ],
-                ),
-                SizedBox(
-                  height: mq.height * .05,
-                ),
-                Container(
-                  height: mq.height * .06,
-                  child: Center(
-                      child: Text(
-                    "Add Property",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold),
-                  )),
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 44, 7, 253),
-                      borderRadius: BorderRadius.circular(20)),
-                )
-              ],
+                  ]),
             ),
           ),
         ),
       ),
-    ));
+    );
   }
 }
